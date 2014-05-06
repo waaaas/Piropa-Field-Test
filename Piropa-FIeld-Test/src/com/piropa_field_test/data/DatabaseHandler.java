da02ca71f5d75063377d5b7e516c1c62797a5f7f
engbers <martin@vollenbroich-web.de>
2014-05-06 16:55:05 +0200

    Database erstellt

--- Piropa-FIeld-Test/src/com/piropa_field_test/data/DatabaseHandler.java
+++ Piropa-FIeld-Test/src/com/piropa_field_test/data/DatabaseHandler.java
@@ -2,7 +2,6 @@ package com.piropa_field_test.data;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -10,13 +9,12 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 	private static final String DATABASE_NAME = "Data.db";
 	private static final int DATABASE_VERSION = 1;
 	
-	private static final String DATA_TABLE = "Data";
+	private static final String DATA_RADIO_MAST_ID = "RADIO_MAST_ID";
+	
 	private static final String DATA_ID = "_ID";
 	private static final String DATA_GPS_X ="X-Axis";
 	private static final String DATA_GPS_Y ="Y-Axis";
-	private static final String DATA_GPS_Z ="Z-Axis";
-	private static final String DATA_2G ="2G";
-	private static final String DATA_CONTENT = "content";
+	private static final String DATA_SIGNAL_VALUE ="Signal_Value";
 	
 	public DatabaseHandler(Context context) {
 		super(context, DATABASE_NAME, null/*factory*/, DATABASE_VERSION);
@@ -26,15 +24,19 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		// TODO Auto-generated method stub
-	/*	String createDB = "CREATE TABLE "+DATA_TABLE+" ("+
-				DATA_ID+" INTEGER PRIMARY KEY, "+ DATA_TITEL
-	 */
+	String createDB = "CREATE TABLE "+DATA_RADIO_MAST_ID+" ("+
+				DATA_ID+" INTEGER PRIMARY KEY, "+ 
+				DATA_GPS_X+ " TEXT,"+
+				DATA_GPS_Y+ " TEXT,"+
+				DATA_SIGNAL_VALUE+ " INTEGER)";
+	db.execSQL(createDB);
+	 
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Auto-generated method stub
-		db.execSQL("DROP TABLE IF EXISTS "+DATA_TABLE);
+		db.execSQL("DROP TABLE IF EXISTS "+DATA_RADIO_MAST_ID);
 		onCreate(db);
 	}
 
